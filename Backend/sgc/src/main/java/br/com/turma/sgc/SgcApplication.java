1a5542349fb9229e9dab8aaf12b1934451676461
Mikael <elkamlive@gmail.com>
2022-02-04 08:39:31 -0300

    Projeto Criado com Spring Initializr

---
+++ Backend/sgc/src/main/java/br/com/turma/sgc/SgcApplication.java
@@ -0,0 +1,13 @@
+package br.com.turma.sgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcApplication.class, args);
+	}
+
+}
