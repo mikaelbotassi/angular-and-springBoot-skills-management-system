abfee07612b1831b759424c3c2f4679e32f9780b
lucas <lucasboeloni@gmail.com>
2022-02-04 21:18:36 -0300

    Tabela, getAll e getById de status pronto

--- Backend/sgc/src/main/java/br/com/turma/sgc/resource/StatusResource.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/resource/StatusResource.java
@@ -3,15 +3,13 @@ package br.com.turma.sgc.resource;
 import br.com.turma.sgc.domain.Status;
 import br.com.turma.sgc.service.StatusService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Fetch;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.Path;
 import java.util.List;
 
 @RequestMapping("api/status")
