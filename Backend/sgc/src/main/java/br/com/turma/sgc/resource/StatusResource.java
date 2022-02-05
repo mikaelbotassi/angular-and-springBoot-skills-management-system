24f54a5d44d294d10e2ac523d5e638ab235e76d3
lucas <lucasboeloni@gmail.com>
2022-02-04 21:14:59 -0300

    Tabela, getAll e getById de status pronto

--- Backend/sgc/src/main/java/br/com/turma/sgc/resource/StatusResource.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/resource/StatusResource.java
@@ -1,24 +1,35 @@
 package br.com.turma.sgc.resource;
 
+import br.com.turma.sgc.domain.Status;
+import br.com.turma.sgc.service.StatusService;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.Fetch;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RequestMapping("/Status")
+import java.nio.file.Path;
+import java.util.List;
+
+@RequestMapping("api/status")
 @RestController
+@RequiredArgsConstructor
 public class StatusResource {
 
+    private final StatusService service;
 
     @GetMapping
-    public ResponseEntity<String> findAll(){
-        return ResponseEntity.ok().body("GET");
+    public ResponseEntity<List<Status>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
     }
 
-
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Status> findById(@PathVariable int id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
 
 
 
