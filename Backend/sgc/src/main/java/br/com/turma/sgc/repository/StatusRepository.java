f9f8645e441e7566ba3fd377a7c87eccefd1a305
lucas <lucasboeloni@gmail.com>
2022-02-06 14:18:39 -0300

    criacao status enum

--- Backend/sgc/src/main/java/br/com/turma/sgc/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.turma.sgc.repository;
-import br.com.turma.sgc.domain.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-
-}
--- Backend/sgc/src/main/java/br/com/turma/sgc/resource/StatusResource.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/resource/StatusResource.java
@@ -1,34 +1,18 @@
 package br.com.turma.sgc.resource;
-
-import br.com.turma.sgc.domain.Status;
 import br.com.turma.sgc.service.StatusService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("api/status")
+@RequestMapping("/api/status")
 @RestController
 @RequiredArgsConstructor
 public class StatusResource {
 
     private final StatusService service;
-
-    @GetMapping
-    public ResponseEntity<List<Status>> findAll(){
-        return ResponseEntity.ok().body(service.findAll());
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Status> findById(@PathVariable int id){
-        return ResponseEntity.ok().body(service.findById(id));
-    }
-
-
 
 }
