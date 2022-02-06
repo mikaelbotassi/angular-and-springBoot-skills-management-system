20553dd45fbc7ddcec034d4949a1c00eff774eff
lucas <lucasboeloni@gmail.com>
2022-02-06 14:18:39 -0300

    criacao status enum

--- Backend/sgc/src/main/java/br/com/turma/sgc/service/StatusService.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/service/StatusService.java
@@ -1,26 +1,11 @@
 package br.com.turma.sgc.service;
 
-import br.com.turma.sgc.domain.Status;
-import br.com.turma.sgc.repository.StatusRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
+@RequiredArgsConstructor
 public class StatusService {
 
-    @Autowired
-    private StatusRepository repository;
-
-    public List<Status> findAll(){
-        return repository.findAll();
-    }
-
-    public Status findById(int id){
-        Optional<Status> obj = repository.findById(id);
-        return obj.get();
-    }
 
 }
