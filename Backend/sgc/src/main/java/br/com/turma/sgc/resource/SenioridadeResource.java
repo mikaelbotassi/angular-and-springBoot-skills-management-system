9fe3a902ca4a617849f692c654d590f1524a3978
Mikael <elkamlive@gmail.com>
2022-02-06 15:35:52 -0300

    Senioridade Funcionando

--- Backend/sgc/src/main/java/br/com/turma/sgc/resource/SenioridadeResource.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/resource/SenioridadeResource.java
@@ -1,7 +1,9 @@
 package br.com.turma.sgc.resource;
 
+import br.com.turma.sgc.enums.SenioridadeEnum;
 import br.com.turma.sgc.service.SenioridadeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/senioridade")
@@ -11,6 +13,14 @@ public class SenioridadeResource {
 
     private final SenioridadeService service;
 
+    @GetMapping
+    public ResponseEntity<SenioridadeEnum[]> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<SenioridadeEnum> findById(@PathVariable int id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
 
 }
