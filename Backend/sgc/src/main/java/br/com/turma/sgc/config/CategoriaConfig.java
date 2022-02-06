83e53cfd862f462fc4fbd280b6a86380a491c329
Layla Couto <laylamscouto@gmail.com>
2022-02-06 10:17:08 -0300

    Categoria

--- Backend/sgc/src/main/java/br/com/turma/sgc/config/CategoriaConfig.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/config/CategoriaConfig.java
@@ -1,14 +1,5 @@
 package br.com.turma.sgc.config;
 
-import br.com.turma.sgc.domain.Categoria;
-import br.com.turma.sgc.services.CategoriaServices;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
 public class CategoriaConfig {
+
 }
