3fc9427f5fd7641413ccc11683f62dd1bba499b5
Layla Couto <laylamscouto@gmail.com>
2022-02-07 16:10:23 -0300

    alteracoes na tabela Colaborador_Competecia com CRUD

--- Backend/sgc/src/main/java/br/com/turma/sgc/domain/Colaborador_Competecia.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/domain/Colaborador_Competecia.java
@@ -1,20 +1,40 @@
 package br.com.turma.sgc.domain;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import br.com.turma.sgc.domain.pk.Colaborador_CompetenciaPK;
+import br.com.turma.sgc.enums.NivelEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "colaborador_competencia")
 public class Colaborador_Competecia implements Serializable {
 
     @EmbeddedId
-    private static final long ColaboradorCompetenciaPK = 1L;
+    private Colaborador_CompetenciaPK id;
 
     @ManyToOne
+    @MapsId("idColborador")
     @JoinColumn(name = "id_colaborador")
     private Colaborador colaborador;
 
     @ManyToOne
+    @MapsId("idCompetencia")
     @JoinColumn(name = "id_competencia")
     private Competencia competencia;
+
+    @Enumerated(EnumType.ORDINAL)
+    private NivelEnum nivel;
+
+    public Colaborador_Competecia(){}
+
+    public Colaborador_Competecia(Colaborador_CompetenciaPK id, Colaborador colaborador, Competencia competencia) {
+        this.id = id;
+        this.colaborador = colaborador;
+        this.competencia = competencia;
+    }
 }
--- Backend/sgc/src/main/java/br/com/turma/sgc/repository/Colaborador_Competencia_Repository.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/repository/Colaborador_Competencia_Repository.java
@@ -1,9 +1,10 @@
 package br.com.turma.sgc.repository;
 
 import br.com.turma.sgc.domain.Colaborador;
+import br.com.turma.sgc.domain.pk.Colaborador_CompetenciaPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Colaborador_Competencia_Repository extends JpaRepository<Colaborador, Integer> {
+public interface Colaborador_Competencia_Repository extends JpaRepository<Colaborador, Colaborador_CompetenciaPK> {
 }
