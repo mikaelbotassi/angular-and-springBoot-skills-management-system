3d6f4e96d075cc43d5e3ebc22d6ee6aae091d99f
Mikael <elkamlive@gmail.com>
2022-02-07 20:32:54 -0300

    Inicio Tabela Status

--- Backend/sgc/src/main/java/br/com/turma/sgc/domain/Status.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/domain/Status.java
@@ -1,4 +1,18 @@
 package br.com.turma.sgc.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "senioridade")
 public class Status {
+
+    @Id
+    private int id;
+
+    @Column(name = "nome")
+    private String nome;
+
 }
