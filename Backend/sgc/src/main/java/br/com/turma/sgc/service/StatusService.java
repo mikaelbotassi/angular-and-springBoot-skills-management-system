445f5bba0ca77f11cf71433cad274ce02eb4d43a
Mikael <elkamlive@gmail.com>
2022-02-07 20:52:22 -0300

    Criação e inserção tabela Status

--- Backend/sgc/src/main/java/br/com/turma/sgc/service/StatusService.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/service/StatusService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class StatusService {
+
     private final StatusRepository repository;
 
     public List<Status> procurarTodos(){
