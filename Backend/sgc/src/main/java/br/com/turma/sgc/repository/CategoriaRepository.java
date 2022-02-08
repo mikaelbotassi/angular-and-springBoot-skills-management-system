548506e0bf48d853c8a1b34a0bd622d3a0718201
lucas <lucasboeloni@gmail.com>
2022-02-08 13:27:36 -0300

    reset

--- Backend/sgc/src/main/java/br/com/turma/sgc/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.turma.sgc.repository;
-
-import br.com.turma.sgc.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
