bd21d996af5e4bd8400a2cc4e21f1524435c0850
Mikael <elkamlive@gmail.com>
2022-02-07 21:53:38 -0300

    Criação e inserção tabela Status

--- Backend/sgc/src/main/java/br/com/turma/sgc/domain/Status.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/domain/Status.java
@@ -1,11 +1,16 @@
 package br.com.turma.sgc.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "senioridade")
 public class Status {
 
--- Backend/sgc/src/main/java/br/com/turma/sgc/service/StatusService.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/service/StatusService.java
@@ -3,7 +3,6 @@ package br.com.turma.sgc.service;
 import br.com.turma.sgc.domain.Status;
 import br.com.turma.sgc.repository.StatusRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
