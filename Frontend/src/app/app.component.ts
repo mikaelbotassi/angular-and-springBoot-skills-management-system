f63c2bca64d72290091a9a57cbe7fb060e7f7f47
lucas <lucasboeloni@gmail.com>
2022-02-17 19:22:48 -0300

    pull de Develops

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -52,7 +52,6 @@ export class AppComponent implements AfterViewInit, OnDestroy, OnInit {
         this.menuService.itens = [
             { label: 'Dashboard', icon: 'dashboard', routerLink: ['/'] },
             { label: 'Turma de formacao', icon: 'book', routerLink: ['/turma'] },
-
             { label: 'Competencias', icon: 'check_circle', routerLink: ['/competencia'] },
             { label: 'Colaboradores', icon: 'supervisor_account', routerLink: ['/colaborador'] }
         ];
