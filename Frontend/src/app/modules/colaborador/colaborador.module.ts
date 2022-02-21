e3fbbd750d6214bc8169b90bf3a3815c7f4462eb
GattoFofo <gu12peruchi@hotmail.com>
2022-02-21 12:12:11 -0300

    Listagem de colaborador funcionando

--- Frontend/src/app/modules/colaborador/colaborador.module.ts
+++ Frontend/src/app/modules/colaborador/colaborador.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ColaboradorRoutingModule } from './colaborador-routing.module';
-import { ColaboradorListaComponent } from './components/colaborador-lista/colaborador-lista/colaborador-lista.component';
+import { ColaboradorListaComponent } from './components/colaborador-lista/colaborador-lista.component';
 
 
 @NgModule({
--- Frontend/src/app/modules/colaborador/service/colaborador.service.ts
+++ Frontend/src/app/modules/colaborador/service/colaborador.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ColaboradorModel} from '../models/ColaboradorModel';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ColaboradorService {
+    private categoriaUrl = 'api/colaborador';
 
-  constructor() { }
+    constructor(
+        private http: HttpClient
+    ) {
+    }
 
-
+    getColaborador(): Observable<ColaboradorModel[]> {
+        return this.http.get<ColaboradorModel[]>(this.categoriaUrl);
+    }
 }
