beacca594c8450db624c98563cbdf26506a76b70
GattoFofo <gu12peruchi@hotmail.com>
2022-02-23 18:28:31 -0300

    add breadcrumb Colaborador add breadcrumb Competencia concertando menu add id to ColaboradorBuscaDTO

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { ColaboradorModule } from './modules/colaborador/colaborador.module';
 const routes: Routes = [
     { path: 'diario-erros', component: DiarioErrosComponent, data: { breadcrumb: 'Diário de Erros'} },
     { path: 'login-success', component: LoginSuccessComponent },
-    { path: 'colaborador', loadChildren : () => ColaboradorModule},
-    {path: 'competencia', loadChildren: () => CompetenciaModule}
+    { path: 'colaborador', loadChildren : () => ColaboradorModule, data: {breadcrumb: 'Colaborador'}},
+    {path: 'competencia', loadChildren: () => CompetenciaModule, data: {breadcrumb: 'Competencia'}}
 ];
 
 @NgModule({
