5e58efca58c860072bd358c28cb07ccdcb69bc15
Mikael <elkamlive@gmail.com>
2022-02-24 19:42:38 -0300

    colaborador funcionando

--- Frontend/src/app/modules/colaborador/service/colaborador.service.ts
+++ Frontend/src/app/modules/colaborador/service/colaborador.service.ts
@@ -2,19 +2,19 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ColaboradorModel} from '../models/ColaboradorModel';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ColaboradorService {
-    private categoriaUrl = 'api/colaborador';
 
-    constructor(
-        private http: HttpClient
-    ) {
+    private url = environment.apiUrl + 'colaborador';
+
+    constructor(private http: HttpClient) {
     }
 
     getColaborador(): Observable<ColaboradorModel[]> {
-        return this.http.get<ColaboradorModel[]>(this.categoriaUrl);
+        return this.http.get<ColaboradorModel[]>(this.url);
     }
 }
