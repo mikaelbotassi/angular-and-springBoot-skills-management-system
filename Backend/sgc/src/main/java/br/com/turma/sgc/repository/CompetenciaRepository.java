944f515e61b0e99a774acc153e7ad524ea027bf4
Mikael <elkamlive@gmail.com>
2022-02-27 02:00:35 -0300

    Validação para não existir competencia com o mesmo nome

--- Backend/sgc/src/main/java/br/com/turma/sgc/repository/CompetenciaRepository.java
+++ Backend/sgc/src/main/java/br/com/turma/sgc/repository/CompetenciaRepository.java
@@ -1,12 +1,14 @@
 package br.com.turma.sgc.repository;
 
 import br.com.turma.sgc.domain.Competencia;
+import br.com.turma.sgc.service.dto.CompetenciaDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -15,5 +17,8 @@ public interface CompetenciaRepository extends JpaRepository<Competencia, Intege
     //Query para pegar as competências de determinada categoria.(Layla) OK
     @Query(value = "select c from Competencia c join Categoria ca on ca.id = :idCategoria")
     List<Competencia> buscarCompetenciaPorIdCategoria(@Param("idCategoria") Integer idCategoria);
+
+    @Query(value = "select c from Competencia c where lower(c.nome) LIKE lower(:#{#competencia.nome})")
+    Optional<Competencia> buscarPorNome(@Param("competencia") CompetenciaDTO competencia);
 
 }
